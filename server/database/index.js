5d044a338a91ea43142434143ab5522ed117a3e5
brkckr20 <burak2043@gmail.com>
2024-01-01 23:50:08 +0300

    linux - mint kurulumu yapıldı

--- server/database/index.js
+++ server/database/index.js
@@ -2,7 +2,7 @@ import mysql from 'mysql';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '1234', //linux mint kurulumundan sonra şifre eklendi
     database: 'auth'
 })
 
